ed368398a9fb7c190d0ae9ab649fb426e2b7cf26
AnthonyOkosun <okosunanthony@gmail.com>
2025-05-25 23:13:02 -0600

    Fourth Attempt

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
-"use strict";
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollElements = document.querySelectorAll(".scroll-fade-in");
 
-/*
-   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
-   Tutorial 14
-   Case Problem 1
+  const elementInView = (el, offset = 100) =>
+    el.getBoundingClientRect().top <= window.innerHeight - offset;
 
-   Author: Emmanuel Okosun 
-   Date:   
-   
-   Filename: bu_bubbles.js
+  const displayScrollElement = (el) => {
+    el.classList.add("visible");
+  };
 
-*/
-
-
-
-
-
-window.addEventListener("load", function() {
-   // Reference to the bubble box
-   var bubbleBox = document.getElementById("bubbleBox");
-   
-   // Create a new bubble every half-second
-   setInterval(function() {
-      
-      // Do not create more than 20 bubbles at any one time
-      if (bubbleBox.childElementCount <= 20) {
-         
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el)) {
+        displayScrollElement(el);
       }
-      
-   }, 500);
+    });
+  };
 
-   /* Function to return a random integer between minVal and maxValue inclusive */
-   function randInt(minVal, maxVal) {
-      var size = maxVal - minVal + 1;
-      return Math.floor(minVal + size*Math.random());
-   }  
+  window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+  });
 
+  handleScrollAnimation(); // run on load
 });
